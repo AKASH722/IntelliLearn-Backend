5007b4fc281a0cb02e6d78b8528936295ca083e7
AKASH RAI <akashrai4546@gmail.com>
2024-04-20 15:48:26 +0530

    Init

---
+++ src/main/java/com/codex/intellilearn/IntelliLearnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codex.intellilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliLearnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliLearnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codex/intellilearn/IntelliLearnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codex.intellilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliLearnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
